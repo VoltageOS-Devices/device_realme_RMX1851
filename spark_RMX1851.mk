15be026589688ec2dd06e2a666b2d5487842d095
SheikhFaisal713 <sheikhfaisal.19jics038@jietjodhpur.ac.in>
2022-01-17 12:23:05 +0530

    RMX1851: Bump to Jan FP

--- spark_RMX1851.mk
+++ spark_RMX1851.mk
@@ -24,8 +24,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1 7957957 release-keys"
-    BUILD_FINGERPRINT="google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
+
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1851" \
