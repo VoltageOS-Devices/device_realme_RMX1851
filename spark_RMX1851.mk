09348b37530fade6969f39d4ad3a7d6311a72cef
SheikhFaisal713 <sheikhfaisal.19jics038@jietjodhpur.ac.in>
2022-07-20 11:48:47 +0530

    RMX1851: Bump to July Raven FP

--- spark_RMX1851.mk
+++ spark_RMX1851.mk
@@ -27,9 +27,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SP3A.220605.009.B1 8650216 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003.A1 8672226 release-keys"
 
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1851" \
